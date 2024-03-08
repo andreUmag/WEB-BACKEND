44d904436c744f1593ea0dd9d4c35d60354555a4
LenisEstevez <you@example.com>
2024-03-08 14:57:52 -0500

    Mensaje & Partida models Added

--- src/main/java/app/webbackend/models/Mensaje.java
+++ src/main/java/app/webbackend/models/Mensaje.java
@@ -1,4 +1,31 @@
 package app.webbackend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mensajes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class Mensaje {
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Long id;
+    private String creador;
+    private String destinatario;
+    private String contenido;
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime create_at;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", nullable = false)
+    private usuarios usuario;
 }
--- src/main/java/app/webbackend/models/Partida.java
+++ src/main/java/app/webbackend/models/Partida.java
@@ -1,4 +1,35 @@
 package app.webbackend.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "partidas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Partida {
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Long id;
+    private String creador;
+    private String deporte;
+    private String ciudad;
+    private String provincia;
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime fecha;
+    @Temporal(TemporalType.TIME)
+    private LocalDateTime hora_comienzo;
+    @Temporal(TemporalType.TIME)
+    private LocalDateTime hora_final;
+    private Integer participantes;
+    private Integer suplentes;
+    private String comentarios;
+
+    @ManyToMany(mappedBy = "partidas")
+    private List<usuarios> usuarios;
 }
